0535d9260456ffc6e5ae16e0ca83cb747d6f0053
ethanauditore <ethan@ciencias.unam.mx>
2023-04-28 01:45:11 -0600

    fix lending migrations

--- resources/migrations/20230425042616-book-lending.up.sql
+++ resources/migrations/20230425042616-book-lending.up.sql
@@ -5,9 +5,9 @@ create table catalog.book_item (
 --;;
 create table catalog.lending (
   lending_id bigint generated always as identity primary key,
-  book_id bigint not null references catalog.book,
-  user_id bigint not null unique,
-  lending_date date,
-  due_date date,
+  book_item_id bigint not null unique references catalog.book_item,
+  user_id bigint not null,
+  lending_date date not null default current_date,
+  due_date date not null default current_date + 14,
   check(due_date - lending_date <= 14)
 );
