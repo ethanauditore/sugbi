add6a65e201d5bc5f8ea5dafd22e06261262bcd5
ethanauditore <ethan@ciencias.unam.mx>
2023-04-28 01:44:34 -0600

    add book items migrations

--- resources/migrations/20230425042616-book-lending.down.sql
+++ resources/migrations/20230425042616-book-lending.down.sql
@@ -1,1 +1,3 @@
 drop table catalog.lending;
+--;;
+drop table catalog.book_item;
--- resources/migrations/20230425042616-book-lending.up.sql
+++ resources/migrations/20230425042616-book-lending.up.sql
@@ -1,3 +1,8 @@
+create table catalog.book_item (
+  book_item_id bigint generated always as identity primary key,
+  book_id bigint not null references catalog.book
+);
+--;;
 create table catalog.lending (
   lending_id bigint generated always as identity primary key,
   book_id bigint not null references catalog.book,
