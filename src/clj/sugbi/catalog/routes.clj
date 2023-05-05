2004c90abfcd78fa1a1c0daa75a90e9f810148b2
ethanauditore <ethan@ciencias.unam.mx>
2023-05-05 17:26:25 -0600

    add loan-spec and routes definition

--- src/clj/sugbi/catalog/routes.clj
+++ src/clj/sugbi/catalog/routes.clj
@@ -24,6 +24,15 @@
    (ds/opt :subjects)            [string?]
    (ds/opt :number-of-pages)     int?})
 
+(defn date?
+  [o]
+  (instance? java.time.LocalDate o))
+
+(def loan-spec
+  {:lending-id     int?
+   :lending-date date?
+   :due-date date?
+   :return-date (ds/maybe date?)})
 
 (def routes
   ["/catalog" {:swagger {:tags ["Catalog"]}}
@@ -38,14 +47,38 @@
                 :responses  {200 {:body basic-book-info-spec}
                              405 {:body {:message string?}}}
                 :handler    catalog.handlers/insert-book!}}]
-    ["/:isbn" {:get    {:summary    "get a book info by its isbn"
-                        :parameters {:path {:isbn string?}}
-                        :responses  {200 {:body book-info-spec}
-                                     404 {:body {:isbn string?}}}
-                        :handler    catalog.handlers/get-book}
-               :delete {:summary    "delete a book title of the catalog"
-                        :parameters {:header {:cookie string?}
-                                     :path   {:isbn string?}}
-                        :responses  {200 {:body {:deleted int?}}
-                                     405 {:body {:message string?}}}
-                        :handler    catalog.handlers/delete-book!}}]]])
+    ["/:isbn" {:parameters {:path {:isbn string?}}}
+     ["" {:get    {:summary    "get a book info by its isbn"
+                   :responses  {200 {:body book-info-spec}
+                                404 {:body {:isbn string?}}}
+                   :handler    catalog.handlers/get-book}
+          :delete {:summary    "delete a book title of the catalog"
+                   :parameters {:header {:cookie string?}}
+                   :responses  {200 {:body {:deleted int?}}
+                                405 {:body {:message string?}}}
+                   :handler    catalog.handlers/delete-book!}}]
+     ["/item"
+      ["/:book-item-id" {:parameters {:path {:book-item-id int?}}}
+       ["/checkout" {:post {:summary "creates a lending as the current user"
+                            :responses {200 {:body loan-spec}
+                                        403 {:body {:message string?}}
+                                        404 {:body {:message string?}}
+                                        409 {:body {:message string?}}}
+                            :handler catalog.handlers/checkout-lending!}}]
+       ["/return" {:post {:summary "creates a lending as the current user"
+                          :responses {200 {:body {:updated int?}}
+                                      403 {:body {:message string?}}
+                                      404 {:body {:message string?}}
+                                      409 {:body {:message string?}}}
+                          :handler catalog.handlers/return-book!}}]]]]]])
+
+(def lending-routes
+  ["/user" {:swagger {:tags ["User"]}}
+   ["/lendings" {:get {:summary    (str "returns all the loans of the user. "
+                                        "Optionally accepts a search "
+                                        "criteria if you are a librarian")
+                       :parameters {:query {(ds/opt :user-id) string?}}
+                       :responses  {200 {:body [loan-spec]}
+                                    403 {:body {:message string?}}
+                                    404 {:body {:message string?}}}
+                       :handler    catalog.handlers/get-book-lendings}}]])
