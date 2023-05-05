c113974c08df63bde068b4b23f5ce7836a32b74e
ethanauditore <ethan@ciencias.unam.mx>
2023-05-05 17:20:01 -0600

    remove book_item_id restriction and change user-id to text

--- resources/migrations/20230425042616-book-lending.up.sql
+++ resources/migrations/20230425042616-book-lending.up.sql
@@ -5,8 +5,8 @@ create table catalog.book_item (
 --;;
 create table catalog.lending (
   lending_id bigint generated always as identity primary key,
-  book_item_id bigint not null unique references catalog.book_item,
-  user_id bigint not null,
+  book_item_id bigint not null references catalog.book_item,
+  user_id text not null,
   lending_date date not null default current_date,
   due_date date not null default current_date + 14,
   check(due_date - lending_date <= 14)
